298c06f49723476f35a5bb11d293ddc114b7879e
Steven <666666>
2018-12-03 16:18:56 +0800

    springboot maven init

--- account/src/main/java/com/github/steven/SpringCloudAccountApplication.java
+++ account/src/main/java/com/github/steven/SpringCloudAccountApplication.java
@@ -1,6 +1,8 @@
 package com.github.steven;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringCloudAccountApplication {
     public static void main(String[] args) {
 
